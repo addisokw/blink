887373c88b83a62c6971821c423672e96daa03ed
Ken Addison <ken@kenaddison.com>
2015-02-09 17:53:00 -0800

    commit arduino code

--- windows/app.js
+++ windows/app.js
@@ -1,3 +1,25 @@
-/**
- * Created by Ken on 2/9/2015.
- */
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("COM3", {
+    baudrate: 9600
+});
+
+serialPort.open(function (error) {
+    if ( error ) {
+        console.log('failed to open: '+error);
+    } else {
+        console.log('open');
+    }
+});
+
+serialPort.on('data', function(data) {
+    console.log(data.toString());
+});
+
+/*
+serialPort.close(function (error) {
+    if ( error ) {
+        console.log('failed to close: ' + error);
+    } else {
+        console.log('closed');
+    }
+});*/
